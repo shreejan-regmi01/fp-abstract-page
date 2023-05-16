a57f2d629b935a167886c94b48d88d7d21a4c5dc
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-05-16 23:41:57 +0545

    added dropdown header

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let dropdownIsDisplayed = false;
 document
   .getElementsByClassName("hamburger")[0]
   .addEventListener("click", () => {
     document.getElementsByClassName("hamburger")[0].classList.toggle("active");
+    // dropdownIsDisplayed = !dropdownIsDisplayed;
+    // document.querySelector(".header__dropdown").style.display =
+    //   dropdownIsDisplayed ? "block" : "none";
+    document.querySelector(".header__dropdown").classList.toggle("display");
+    document.querySelector(".header__dropdown").classList.toggle("hide");
   });
