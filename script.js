27ab46b65763573675c19a8f43deedd5fb853607
Shreejan Regmi <shreejan.regmi@gmail.com>
2023-05-18 20:31:15 +0545

    added search overlay

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ document
     document.querySelector(".header__dropdown").classList.toggle("display");
     document.querySelector(".header__dropdown").classList.toggle("hide");
   });
+
+document.querySelector(".search-close-icon").addEventListener("click", (e) => {
+  document.querySelector(".search-bar-overlay").classList.toggle("hide");
+});
+
+document
+  .querySelector(".header__search-icon")
+  .addEventListener("click", (e) => {
+    document.querySelector(".search-bar-overlay").classList.toggle("hide");
+  });
